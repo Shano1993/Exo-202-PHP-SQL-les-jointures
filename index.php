7439e8ddb75becdc34d4935bff5b6f61f4d46be1
Shano1993 <stefan.hanotiau@gmail.com>
2022-02-25 10:17:16 +0100

    php

--- index.php
+++ index.php
@@ -18,6 +18,32 @@
 
 // TODO Votre code ici.
 
+try {
+    $server = 'localhost';
+    $db = 'blog';
+    $user = 'root';
+    $password = '';
+
+    $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+    //Requête sans alias
+    $request = $pdo->prepare("
+        SELECT article.id, article.title, article.content, categorie.name
+        FROM article
+        INNER JOIN categorie ON category_fk = categorie.id
+    ");
+
+    if ($request->execute()) { ?>
+        <p>Sans les alias</p> <?php
+        foreach ($request->fetchAll() as $value) {
+            echo "L'id de l'article : " . $value['id'] . "<br>" .
+                 "Le titre de l'article : " . $value['title'] . "<br>" .
+                 "Le contenu de l'article : " . $value['content'] . "<br>" .
+                 "Le nom de l'article : " . $value['name'] . "<br>";
+        }
+    }
 
 /**
  * 4. Réalisez la même chose que le point 3 en utilisant un maximum d'alias.
@@ -25,6 +51,21 @@
 
 // TODO Votre code ici.
 
+    $request = $pdo->prepare("
+        SELECT ar.id, ar.title, ar.content, ca.name
+        FROM article as ar
+        INNER JOIN categorie as ca ON category_fk = ca.id
+    ");
+
+    if ($request->execute()) { ?>
+        <p>Avec les alias</p> <?php
+        foreach ($request->fetchAll() as $value) {
+            echo "L'id de l'article : " . $value['id'] . "<br>" .
+                "Le titre de l'article : " . $value['title'] . "<br>" .
+                "Le contenu de l'article : " . $value['content'] . "<br>" .
+                "Le nom de l'article : " . $value['name'] . "<br>";
+        }
+    }
 
 /**
  * 5. Ajoutez un utilisateur dans la table utilisateur.
@@ -33,3 +74,22 @@
  */
 
 // TODO Votre code ici.
+
+    $request = $pdo->prepare("
+        SELECT commentaire.content, utilisateur.firstName, utilisateur.lastName
+        FROM commentaire
+        LEFT JOIN utilisateur ON user_fk = utilisateur.id
+    ");
+
+    if ($request->execute()) { ?>
+        <p>Les commentaires écrit par les utilisateurs</p> <?php
+        foreach ($request->fetchAll() as $value) {
+            echo "Le commentaire : " . $value['content'] . "<br>" .
+                 "Le firstname : " . $value['firstName'] . "<br>" .
+                 "Le lastname : " . $value['lastName'] . "<br>";
+        }
+    }
+}
+catch (Exception $exception) {
+    echo $exception->getMessage();
+}
